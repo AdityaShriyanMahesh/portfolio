3b4f5b658773d8270d9d4f1cc598dbc96432eced
AdityaShriyanMahesh <adityashriyanmahesh@gmail.com>
2023-06-30 10:19:43 +0530

    website

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html data-bs-theme="light" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Aditya Shriyan</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,700,800&amp;display=swap">
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md sticky-top" data-bs-theme="light" style="color: rgb(216,218,224);position: static;">
+        <div class="container-fluid"><a class="navbar-brand" href="#" style="color: var(--bs-body-bg);">ADITYA SHRIYAN</a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse text-center" id="navcol-1" style="color: rgb(216, 218, 224);position: relative;">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link active fs-3 fw-bold" href="index.html" style="color: var(--bs-body-bg);">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="aboutme.php" style="color: var(--bs-body-bg);">About Me</a></li>
+                    <li class="nav-item"><a class="nav-link" href="projects.html" style="color: var(--bs-body-bg);">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="qualification.html" style="color: var(--bs-body-bg);">Qualifications</a></li>
+                    <li class="nav-item"><a class="nav-link" href="Achievements.html" style="color: var(--bs-body-bg);">Achievements</a></li>
+                    <li class="nav-item"><a class="nav-link" href="gallery.html" style="color: var(--bs-body-bg);">Gallery</a></li>
+                    <li class="nav-item"><a class="nav-link" href="hobbies.html" style="color: var(--bs-body-bg);">Hobbies</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.html" style="color: var(--bs-body-bg);">Contact Me</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="container d-flex flex-column justify-content-end" style="color: var(--bs-body-bg);height: 600px;">
+            <div data-bss-parallax-bg="true" style="height: 600px;background-image: url(&quot;assets/img/oliver-spicer-_XK466FNjV8-unsplash.jpg&quot;);background-position: center;background-size: cover;width: 100%;">
+                <h1 class="display-3 fw-bold text-light">Aditya Shriyan</h1>
+                <p class="fs-2 text-info d-flex flex-column justify-content-end align-items-start text-badge mb-5" style="width: 400px;">Computer Science Engineer</p>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section class="py-5">
+            <div class="container py-5">
+                <section>
+                    <div></div>
+                </section>
+                <p class="fs-3 py-5" style="font-size: 21px;background: var(--bs-dark);color: var(--bs-light-bg-subtle);opacity: 1;font-family: 'Work Sans', sans-serif;">I'm Aditya Shriyan, a passionate Computer Science Engineering student. Explore my projects and witness my love for coding and problem-solving. Let's innovate together!</p>
+                <ul class="list-inline fs-1 text-center me-2 py-5" style="padding: 2em 2em;">
+                    <li class="list-inline-item"><a href="https://twitter.com/aditya_shriyan"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-twitter" style="color: var(--bs-secondary-bg);">
+                                <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+                            </svg></a><a href="https://twitter.com/aditya_shriyan"></a></li>
+                    <li class="list-inline-item"><a href="https://www.instagram.com/adityashriyan/"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-instagram" style="color: var(--bs-secondary-bg);">
+                                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
+                            </svg></a></li>
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/aditya-shriyan-a72991225/" style="color: var(--bs-tertiary-bg);"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-linkedin">
+                                <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"></path>
+                            </svg></a></li>
+                </ul>
+            </div>
+        </section>
+        <div class="container">
+            <div class="banner">
+                <p class="fs-1" style="padding: 2em 1em;text-align: center;background: var(--bs-info);border-radius: 1em;">Click here to know more about me.&nbsp;<a class="btn btn-dark btn-lg text-uppercase fw-bold" role="button" data-bss-hover-animate="jello" href="aboutme.php">About Me&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-right fs-2">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                        </svg></a></p>
+                <p class="fs-1" style="padding: 2em 1em;text-align: center;background: var(--bs-info);border-radius: 1em;">Interested in my Projects?&nbsp;<a class="btn btn-dark btn-lg text-uppercase fw-bold" role="button" data-bss-hover-animate="jello" href="projects.html">projects&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-right fs-2">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                        </svg></a></p>
+                <p class="fs-1" style="padding: 2em 1em;text-align: center;background: var(--bs-info);border-radius: 1em;">My Qualifications&nbsp;<a class="btn btn-dark btn-lg text-uppercase fw-bold" role="button" data-bss-hover-animate="jello" href="qualification.html">qualification&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-right fs-2">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                        </svg></a></p>
+                <p class="fs-1" style="padding: 2em 1em;text-align: center;background: var(--bs-info);border-radius: 1em;">My Achievements&nbsp;<a class="btn btn-dark btn-lg text-uppercase fw-bold" role="button" data-bss-hover-animate="jello" href="Achievements.html">Achievements&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-right fs-2">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                        </svg></a></p>
+                <p class="fs-1" style="padding: 2em 1em;text-align: center;background: var(--bs-info);border-radius: 1em;">My Gallery&nbsp;<a class="btn btn-dark btn-lg text-uppercase fw-bold" role="button" data-bss-hover-animate="jello" href="gallery.html">gallery<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-chevron-right fs-2">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+                        </svg></a>&nbsp;</p>
+            </div>
+        </div>
+        <section class="py-5"></section>
+    </main>
+    <section id="client-date" class="fs-2 text-center" style="color: white;">
+        <?php
+        echo "<p>The Time is: ";
+        echo date("h:i:s P");
+        ?>
+</section>
+<section id="client-date" class="fs-2 text-center" style="color: white;">
+<?php
+        $a = file_get_contents("counter.txt");
+        file_put_contents("counter.txt",$a+1);
+        echo "<h1> Count Page</h1>";
+        echo "<p>Total Number of vistiors: $a</p>";
+?>
+</section>
+    <footer class="fs-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div></div>
+                    <p class="fs-2 text-center" style="padding-top: 15%;">CREATED BY: Aditya Shriyan</p>
+                    <p></p>
+                </div>
+                <div class="col-md-6">
+                    <ul class="list-inline fs-1 text-center me-2 py-5" style="padding: 2em 2em;">
+                        <li class="list-inline-item"><a href="https://twitter.com/aditya_shriyan"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-twitter" style="color: var(--bs-secondary-bg);">
+                                    <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"></path>
+                                </svg></a><a href="https://twitter.com/aditya_shriyan"></a></li>
+                        <li class="list-inline-item"><a href="https://www.instagram.com/adityashriyan/"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-instagram" style="color: var(--bs-secondary-bg);">
+                                    <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"></path>
+                                </svg></a></li>
+                        <li class="list-inline-item"><a href="https://www.linkedin.com/in/aditya-shriyan-a72991225/" style="color: var(--bs-tertiary-bg);"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-linkedin">
+                                    <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"></path>
+                                </svg></a></li>
+                    </ul>
+                    <section><a href="https://atria.edu/assets/pdf/ise/depart_infra/MAD(18CSMP68)%20LAB%20MANUAL.pdf"></a></section>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/bold-and-bright.js"></script>
+    <script>
+        function showdate2()
+        {
+            var d = new Date();
+            var h = d.getHours();
+            var m = d.getMinutes();
+            var s =d.getSeconds();
+            document.getElementById("client-date").innerText = (h + ':' + m + ':' + s);
+            console.log("called");
+            setTimeout(showdate2,1000);
+        }
+        showdate2();
+    </script>
+</body>
+
+</html>
